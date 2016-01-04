56f1c4fe4714477f16c7cbaddce9b333d09b575f
Nicolas Serrano <itnico.las.me@gmail.com>
2016-01-04 22:57:36 +0200

    First Gulpfile with watchers and Sass. Pending: Template engines (Handlebars, Jade?), Image Optimization, Bower Building, Concat Javascript

---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+// ///////////////////////
+// Required
+// ///////////////////////
+
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass'),
+	plumber = require('gulp-plumber'),
+	autoprefixer = require('gulp-autoprefixer'),
+	del = 	require('del'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload,
+	rename = require('gulp-rename');
+
+// ///////////////////////
+// Scripts Tasks
+// ///////////////////////
+
+gulp.task('scripts', function() {
+	gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+		.pipe(plumber())
+		.pipe(rename({suffix:'.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'))
+		.pipe(reload({stream:true}));
+});
+
+// ///////////////////////
+// Sass Tasks
+// ///////////////////////
+
+gulp.task('sass', function() {
+	gulp.src('app/scss/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 versions'))
+		.pipe(gulp.dest('app/css/'))
+		.pipe(reload({stream:true}));
+});
+
+// ///////////////////////
+// Html Tasks
+// ///////////////////////
+
+gulp.task('html', function() {
+	gulp.src('app/**/*.html')
+		.pipe(gulp.dest('app/'))
+		.pipe(reload({stream:true}));
+});
+
+// ///////////////////////
+// Browser-sync Tasks
+// ///////////////////////
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: "app"
+		}
+	});
+});
+
+gulp.task('build:serve', function() {
+	browserSync({
+		server: {
+			baseDir: "build"
+		}
+	});
+});
+
+// ///////////////////////
+// Watch Tasks
+// ///////////////////////
+
+gulp.task('watch', function() {
+	gulp.watch('app/js/**/*.js', ['scripts']);
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/**/*.html', ['html']);
+});
+
+// ///////////////////////
+// Build Tasks
+// ///////////////////////
+
+// Task to clear all the files and folders from build folder
+
+gulp.task('build:cleanfolder', function() {
+	del([
+		'build/**'
+	]);
+});
+
+// Task to create build directory for all files
+
+gulp.task('build:copy', ['build:cleanfolder'], function() {
+	return gulp.src('app/**/*')
+	.pipe(gulp.dest('build/'));
+})
+
+// Task to remove all unwanted files from the build
+
+gulp.task('build:remove', ['build:copy'],  function(cb) {
+	del([
+		'build/scss/',
+		'build/js/!(*.min.js)'
+	], cb);
+});
+
+gulp.task('build', ['build:copy', 'build:remove']);
+
+// ///////////////////////
+// Default Task
+// ///////////////////////
+
+gulp.task('default', ['scripts', 'watch', 'sass', 'html', 'browser-sync']);
